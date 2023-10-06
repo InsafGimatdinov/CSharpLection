e6fe7cede6dd976b3553d9b9d72e861a95d16c4e
Insaf Gimatdinov <insaf.gimatdinov@mail.ru>
2023-10-06 19:13:15 +0300

    Add file Example 6

---
+++ Example6/Program.cs
@@ -0,0 +1,11 @@
+﻿// Пользуемся условными операторами :)
+Console.WriteLine("Enter username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha") {
+    Console.WriteLine("Yes, this is Masha");
+}
+else {
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
