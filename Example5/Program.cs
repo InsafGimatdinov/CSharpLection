af1aa5453459c1684694d57898e1cb0acca5e25f
Insaf Gimatdinov <insaf.gimatdinov@mail.ru>
2023-10-06 19:04:08 +0300

    Add file Example 5

---
+++ Example5/Program.cs
@@ -0,0 +1,8 @@
+﻿// Начинаем пользоваться случайными числами. Числа выпадают следующим образом первое минимальное число
+// входит в промежуток а последнее будет на единичку меньше.
+int numberA = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine("NumberA = " + numberA);
+Console.WriteLine("NumberB = " + numberB);
+int sum  = numberA + numberB;
+Console.WriteLine("sum = " + sum);
