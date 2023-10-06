b0ce2f9a7d472b92e95ebd8be006c43a7f051372
Insaf Gimatdinov <insaf.gimatdinov@mail.ru>
2023-10-06 16:59:54 +0300

    Add file Example2

---
+++ Example2/Program.cs
@@ -0,0 +1,4 @@
+﻿// Второе задание попробовать ввести имя пользоателя :)
+Console.WriteLine("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+Console.WriteLine("Привет, " + userName);
